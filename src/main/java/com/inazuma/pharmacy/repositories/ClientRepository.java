5637e6d4d0077eb7757eafe4adec514b419ac961
Ryan <ryanfelipe.rr@gmail.com>
2024-05-16 23:25:58 -0300

    Client repository created

---
+++ src/main/java/com/inazuma/pharmacy/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.inazuma.pharmacy.repositories;
+
+import com.inazuma.pharmacy.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
