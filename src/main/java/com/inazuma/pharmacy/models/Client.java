151dae60c1cd677243957f4b6cdb8ca4747b29ea
Ryan <ryanfelipe.rr@gmail.com>
2024-05-16 23:08:49 -0300

    Client model

---
+++ src/main/java/com/inazuma/pharmacy/models/Client.java
@@ -0,0 +1,27 @@
+package com.inazuma.pharmacy.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+}
